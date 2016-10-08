d8f61686ab3a0a9ec3461ba4a53aaf43126c59c1
zaddan <behzadboro@gmail.com>
2016-10-08 18:51:02 -0500

    jpg_float added

---
+++ jpg_float/build/CMakeFiles/simple.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/BookKeeping/fp_helpers.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/BookKeeping/fp_helpers.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/BookKeeping/globals.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/BookKeeping/globals.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/BookKeeping/operandFile_parser.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/BookKeeping/operandFile_parser.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/BookKeeping/operatorFile_parser.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/BookKeeping/operatorFile_parser.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/BookKeeping/setSubType.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/BookKeeping/setSubType.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/CSrc/loadjpg.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/CSrc/loadjpg.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/CSrc/main.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/CSrc/main.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/CSrc/savejpg.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/CSrc/savejpg.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/Operators/analysis.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/Operators/analysis.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/Operators/bam.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/Operators/bam.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/Operators/bta.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/Operators/bta.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/Operators/btm.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/Operators/btm.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/Operators/error_model.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/Operators/error_model.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/Operators/error_model_old.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/Operators/error_model_old.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/Operators/fft.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/Operators/fft.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/Operators/hw_ac.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/Operators/hw_ac.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/Operators/io_stat.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/Operators/io_stat.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/Operators/jpeg_helper.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/Operators/jpeg_helper.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/Operators/operand_gen.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/Operators/operand_gen.cpp.o"
+  "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/src/Operators/testing_operators.cpp" "/home/polaris/behzad/behzad_local/approximated_algorithms/jpg/build/CMakeFiles/simple.dir/src/Operators/testing_operators.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../inc/BookKeeping"
+  "../inc/OperatorHeaders"
+  "../inc/CSrc_inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
